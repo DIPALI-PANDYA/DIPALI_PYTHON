9baf38ecd54c756d570dd384841d6be3287fe5bf
DIPALI-PANDYA <125249517+DIPALI-PANDYA@users.noreply.github.com>
2023-02-17 10:30:02 +0530

    Create Assignment 1.py

---
+++ ASSIGNMENT/Assignment 1.py
@@ -0,0 +1,19 @@
+"""
+#WAP sum of first n +ve integers
+n = int(input("Enter the No.:"))
+sum=0
+for i in range(0,n+1):
+    sum=sum+i
+    n=n-1
+
+
+print("Sum:",sum)
+
+
+#WAP to find out if the number is positive or negative
+n = int(input("Enter the No.:"))
+if n>0:
+    print("No. is positive")
+else:
+    print("No. is negative")
+"""
